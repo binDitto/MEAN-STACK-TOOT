a0f4aa5a6dc2a512730c7616a9872ff5129f28d4
Bin Tran <sudocode.bin@gmail.com>
2017-11-15 00:09:35 -0800

    Setting up base app

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  //Can add page not found component page
+  { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(appRoutes)],
+  providers: [],
+  bootstrap: [],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
